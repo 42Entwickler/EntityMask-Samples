e627db7a4298781be06c640814e476c76415fc5a
Bernhard Wurm <bernhard@raupes.net>
2025-05-23 07:41:11 +0200

    Updated comments

--- WebAPIDemo/Models/Project.cs
+++ WebAPIDemo/Models/Project.cs
@@ -5,19 +5,26 @@ namespace WebAPIDemo.Models;
 /// <summary>
 /// Adding the Api Mask Name here at the project and true to the deep-masking-parameter.
 /// So we also get the ApiUser-Mask for the Users in the Project. For the Owner and also for the Users-Collection.
+/// DTO as Protection Proxy build by SourceGen depending on the <see cref="EntityMaskAttribute"/>.
 /// </summary>
 [EntityMask("Api", true)]
 [EntityMask("ApiList", true)]
 public class Project {
     public int Id { get; set; }
+    
     public string? Title { get; set; }
+
     [Mask("ApiList")]
     public string? Description { get; set; }
+    
     public DateTime Start { get; set; }
+    
     [RenameInMask("End", "ApiList")]
     public DateTime PlanedEnd { get; set; }
+    
     [Mask("ApiList")]
     public User? Owner { get; set; }
+    
     [Mask("ApiList")]
     [Mask("Api")]
     public List<User>? Users { get; set; }
